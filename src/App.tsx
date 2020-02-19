51a493579f43efda570e82d4a171bdb8b18948d4
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2020-02-19 08:40:11 -0800

    adding chakra, basic grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import {
+  ThemeProvider,
+  CSSReset,
+  Stack,
+  Box,
+  Grid,
+  Heading,
+  Text
+} from "@chakra-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <CSSReset />
+      <Grid templateColumns="70% 30%" gap={6} p={6}>
+        <Box w="100%" h="10">
+          <Stack spacing={8}>
+            <Box p={5} shadow="md" borderWidth="1px" flex="1" rounded="md">
+              <Heading fontSize="xl">Meeting name</Heading>
+              <Text mt={4}>Lorem ipsum</Text>
+            </Box>
+            <Box p={5} shadow="md" borderWidth="1px" flex="1" rounded="md">
+              <Heading fontSize="xl">Meeting name</Heading>
+              <Text mt={4}>Lorem ipsum</Text>
+            </Box>
+          </Stack>
+        </Box>
+        <Box w="100%" h="10">
+          This is a test.
+        </Box>
+      </Grid>
+    </ThemeProvider>
   );
 }
 
