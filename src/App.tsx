15afc781bb765a89b493fe2981517e84c5ba442c
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2020-02-19 20:34:25 -0800

    adding some checkboxes, data in json format

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,58 @@ import {
   CSSReset,
   Stack,
   Box,
+  CheckboxGroup,
+  Checkbox,
+  FormControl,
+  FormLabel,
   Grid,
   Heading,
-  Text
+  Text,
+  Divider
 } from "@chakra-ui/core";
 
+import * as meetings from "./meetings.json";
+
 function App() {
+  const formats = {
+    email: "Email",
+    chat: "Chat",
+    video: "Video",
+    phone: "Phone",
+    forum: "Forum"
+  };
+  const formatsKeys = Object.keys(formats);
+
+  console.log(meetings);
+
   return (
     <ThemeProvider>
       <CSSReset />
       <Grid templateColumns="70% 30%" gap={6} p={6}>
         <Box w="100%" h="10">
           <Stack spacing={8}>
-            <Box p={5} shadow="md" borderWidth="1px" flex="1" rounded="md">
-              <Heading fontSize="xl">Meeting name</Heading>
-              <Text mt={4}>Lorem ipsum</Text>
-            </Box>
-            <Box p={5} shadow="md" borderWidth="1px" flex="1" rounded="md">
-              <Heading fontSize="xl">Meeting name</Heading>
-              <Text mt={4}>Lorem ipsum</Text>
-            </Box>
+            {meetings.map(meeting => (
+              <Box p={5} shadow="md" borderWidth="1px" flex="1" rounded="md">
+                <Heading fontSize="xl">Meeting name</Heading>
+                <Text mt={4}>Lorem ipsum</Text>
+              </Box>
+            ))}
           </Stack>
         </Box>
         <Box w="100%" h="10">
-          This is a test.
+          <FormControl as="fieldset">
+            <FormLabel as="legend" fontSize="lg" fontWeight="bold">
+              Format
+            </FormLabel>
+            <Divider />
+            <CheckboxGroup variantColor="green" defaultValue={formatsKeys}>
+              {formatsKeys.map(format => (
+                <Checkbox value={format}>
+                  {formats[format as keyof typeof formats]}
+                </Checkbox>
+              ))}
+            </CheckboxGroup>
+          </FormControl>
         </Box>
       </Grid>
     </ThemeProvider>
