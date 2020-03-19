0fbb3987e5f49f5c67e370f6aa409a2ee37fc68c
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2020-03-18 19:10:28 -0700

    removing gray from loading screen

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,13 +3,7 @@ import { Box, Spinner } from "@chakra-ui/core";
 
 export function Loading() {
   return (
-    <Box
-      alignItems="center"
-      bg="gray.50"
-      d="flex"
-      height="100%"
-      justifyContent="center"
-    >
+    <Box alignItems="center" d="flex" height="100%" justifyContent="center">
       <Spinner size="xl" />
     </Box>
   );
