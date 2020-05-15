85395afb8e3484076417c2349df5e94564ed2b1c
RiunioniAAonline <65255639+RiunioniAAonline@users.noreply.github.com>
2020-05-15 18:38:32 +0100

    Update Textual.ts

--- src/components/Textual.ts
+++ src/components/Textual.ts
@@ -4,8 +4,7 @@ import { Text } from '@chakra-ui/core';
 export function Textual() {
   return (
     <Text>
-      Per aggiornare le informazioni scrivere un'email
-      a <a href="mailto:riunioniaaonline@gmail.com">riunioniaaonline@gmail.com</a>
+      Per aggiornare le informazioni scrivere un email a <a href="mailto:riunioniaaonline@gmail.com">riunioniaaonline@gmail.com</a>
     </Text>
   );
 }
