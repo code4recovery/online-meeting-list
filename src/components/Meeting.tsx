e62db78854dd148f2b4396ea51e8727314b89869
RiunioniAAonline <65255639+RiunioniAAonline@users.noreply.github.com>
2020-05-16 10:28:46 +0100

    Use 24-hour format

--- src/components/Meeting.tsx
+++ src/components/Meeting.tsx
@@ -51,7 +51,7 @@ export function Meeting({
             fontWeight="normal"
             ml={{ lg: 2 }}
           >
-            {!meeting.time ? 'Ongoing' : meeting.time.format('dddd h:mm a')}
+            {!meeting.time ? 'Ongoing' : meeting.time.format('dddd HH:mm')}
           </Heading>
         </Box>
         <Box>
