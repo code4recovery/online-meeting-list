00f4a3718b73af3349ebcd63653e85cb0d0f3518
Keith Prinkey <kaprinkey1@cougars.ccis.edu>
2022-04-16 13:08:23 -0500

    Update config.ts

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -1,9 +1,9 @@
 //don't change this 👇 -- see README.md for help creating an .env file for your app
 const sheetUrl = process.env.REACT_APP_GOOGLE_SHEET
   ? process.env.REACT_APP_GOOGLE_SHEET
-  : 'https://docs.google.com/spreadsheets/d/1wER2LP3dT_6_LEQ8fSY1rv2bGzIZ2aaMBi_0Bt1aN3I/edit#gid=0';
+  : 'https://docs.google.com/spreadsheets/d/1Flzy3t6Ph86HwtprT1KnXdB35AayC8iSJZ4Uyv223oM/edit?usp=sharing';
 
-export const dataUrl =
+export const dataUrl = 
   process.env.REACT_APP_JSON_URL ||
   `https://sheets.googleapis.com/v4/spreadsheets/${
     sheetUrl.split('/')[5]
