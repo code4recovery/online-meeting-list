192f2f2d18993c35111750f2c90e163c2fb5cecf
JDV Main Laptop <websup@suffolkny-aa.org>
2025-01-11 11:14:47 -0500

    Hardcode light-theme for now

--- src/helpers/theme.ts
+++ src/helpers/theme.ts
@@ -1,6 +1,7 @@
 import { extendTheme } from '@chakra-ui/react';
 
-const initialColorMode = process.env.REACT_APP_COLOR_MODE ?? 'system';
+// const initialColorMode = process.env.REACT_APP_COLOR_MODE ?? 'system';
+const initialColorMode = process.env.REACT_APP_COLOR_MODE ?? 'light';
 
 export const theme = extendTheme({
   config: {
