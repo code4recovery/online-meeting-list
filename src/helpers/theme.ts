15e6b930a340c7da46a709fb851792d6c155591f
JDV Main Laptop <websup@suffolkny-aa.org>
2025-01-11 11:43:50 -0500

    hardcode "light" , 4th attempt

--- src/helpers/theme.ts
+++ src/helpers/theme.ts
@@ -1,8 +1,12 @@
 import { extendTheme } from '@chakra-ui/react';
 
+// const initialColorMode = process.env.REACT_APP_COLOR_MODE ?? 'system';
+const initialColorMode = 'light';
+
 export const theme = extendTheme({
   config: {
     initialColorMode,
-    useSystemColorMode: 'light'
+    // useSystemColorMode: initialColorMode === 'system'
+    useSystemColorMode: true
   }
 });
