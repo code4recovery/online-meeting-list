5f29de88898270e0965601f5c55d546ffee0deba
JDV Main Laptop <websup@suffolkny-aa.org>
2025-01-19 12:12:53 -0500

    complete roll-back hardcoding of light mode

--- src/helpers/theme.ts
+++ src/helpers/theme.ts
@@ -1,12 +1,10 @@
 import { extendTheme } from '@chakra-ui/react';
 
 const initialColorMode = process.env.REACT_APP_COLOR_MODE ?? 'system';
-// const initialColorMode = 'light';
 
 export const theme = extendTheme({
   config: {
     initialColorMode,
     useSystemColorMode: initialColorMode === 'system'
-    // useSystemColorMode: false
   }
 });
