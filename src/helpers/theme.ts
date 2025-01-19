6968fe6ed5b060741979097afc88a19aa7589676
JDV Main Laptop <websup@suffolkny-aa.org>
2025-01-19 11:59:21 -0500

    roll back hard-coding of light mode

--- src/helpers/theme.ts
+++ src/helpers/theme.ts
@@ -1,12 +1,12 @@
 import { extendTheme } from '@chakra-ui/react';
 
-// const initialColorMode = process.env.REACT_APP_COLOR_MODE ?? 'system';
-const initialColorMode = 'light';
+const initialColorMode = process.env.REACT_APP_COLOR_MODE ?? 'system';
+// const initialColorMode = 'light';
 
 export const theme = extendTheme({
   config: {
     initialColorMode,
-    // useSystemColorMode: initialColorMode === 'system'
-    useSystemColorMode: false
+    useSystemColorMode: initialColorMode === 'system'
+    // useSystemColorMode: false
   }
 });
